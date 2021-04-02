97f1bdea913b24a0caef5b7ad09c2b30b6f514f5
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-01 22:01:18 -0500

    working on js

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,7 @@
-
+var currentHour=moment().format("hA"); 
+var hour=$(".hour");
+console.log(hour);
+var i=0;
 
 
 // Setting the date in the head
@@ -6,3 +9,15 @@ var today = moment();
 $("#currentDay").text(today.format("MMMM Do, YYYY"));
 
 
+$('.hour').each(function(i, $(this)){
+       if(hour.value ===currentHour){
+          hour.addClass(".present") 
+          i++;
+       }else if(hour.value > currentHour){
+        hour.addClass(".future") 
+        i++;
+       }else if(hour.value < currentHour){
+        hour.addClass(".past") 
+        i++;
+       };
+   };
