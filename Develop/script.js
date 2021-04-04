30b9a90dc9a168630d9acc57c57e69f04704bbe0
Joseph Greenwell <joeygreenwell@ymail.com>
2021-04-03 21:24:50 -0500

    adding in use of data type for future improvements

--- Develop/script.js
+++ Develop/script.js
@@ -31,15 +31,15 @@ $(document).ready(function(){
         localStorage.setItem(todo, task);
     };
     function todoUpdate(){
-        $("#9AM").val(localStorage.getItem("9AM"));
-        $("#10AM").val(localStorage.getItem("10AM"));
-        $("#11AM").val(localStorage.getItem("11AM"));
-        $("#12PM").val(localStorage.getItem("12PM"));
-        $("#1PM").val(localStorage.getItem("1PM"));
-        $("#2PM").val(localStorage.getItem("2PM"));
-        $("#3PM").val(localStorage.getItem("3PM"));
-        $("#4PM").val(localStorage.getItem("4PM"));
-        $("#5PM").val(localStorage.getItem("5PM"));
+        $("#9AM").val(localStorage.getItem("9"));
+        $("#10AM").val(localStorage.getItem("10"));
+        $("#11AM").val(localStorage.getItem("11"));
+        $("#12PM").val(localStorage.getItem("12"));
+        $("#1PM").val(localStorage.getItem("13"));
+        $("#2PM").val(localStorage.getItem("14"));
+        $("#3PM").val(localStorage.getItem("15"));
+        $("#4PM").val(localStorage.getItem("16"));
+        $("#5PM").val(localStorage.getItem("17"));
     };
     todoUpdate();
     
